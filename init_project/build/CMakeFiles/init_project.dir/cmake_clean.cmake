231429ef055815615b6563eb1af3b247e50a6247
htrq <119476945+htrq@users.noreply.github.com>
2023-02-13 14:16:21 +0300

    used mingw ganerator

---
+++ init_project/build/CMakeFiles/init_project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/init_project.dir/main.cpp.obj"
+  "CMakeFiles/init_project.dir/main.cpp.obj.d"
+  "init_project.exe"
+  "init_project.exe.manifest"
+  "init_project.pdb"
+  "libinit_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/init_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
