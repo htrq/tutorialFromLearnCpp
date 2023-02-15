2f83fb51dc6082d236c583561a30168749783bd0
htrq <119476945+htrq@users.noreply.github.com>
2023-02-15 16:18:46 +0300

    tried direct initialzation

--- unnamed_project/main.cpp
+++ unnamed_project/main.cpp
@@ -4,4 +4,12 @@ using namespace std;
 int main() {
     cout << "Hello world!" << endl; //this is the first row
     cout << "How r u?" << endl;     //this is the second row
+
+    //here is "direct initialzation"
+    int c(6);   
+    cout << c << endl;
+
+    //in other side is "copy initialzation"
+    int b = 5;
+    cout << b << endl;
 }
