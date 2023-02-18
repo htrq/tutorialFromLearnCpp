94d579ac6df1f2f4c90e9e11da6f5e9d15b20fa0
htrq <119476945+htrq@users.noreply.github.com>
2023-02-18 16:08:54 +0300

    written simple program

--- unnamed_project/main.cpp
+++ unnamed_project/main.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
+int main() {
+    int x{};
+
     cout << "Hello " "world\n" "Hello world\n"; //it also work with many quotes
+    cout << "Enter number value: ";
+    cin >> x;
+    x *= 2;
+    cout << "Doubled entered value is equal to: " << x << "\n";
+
     return 0;
 }
