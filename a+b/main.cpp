f3788ac8dae3540341d3f9a18086395cf927003d
htrq <119476945+htrq@users.noreply.github.com>
2023-02-18 17:02:34 +0300

    written simple calculation program

---
+++ a+b/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a{ }, b{ };
+    cout << "Enter two integers: \n";
+    cin >> a >> b;
+    cout << "Result of multiplication is: " << a * b << "\n";
+    cout << "Result of division is: " << a / b << "\n";
+}
