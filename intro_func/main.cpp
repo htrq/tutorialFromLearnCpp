8fe3134d28dff7d1329b18d5d11ef4a8f5135a05
htrq <119476945+htrq@users.noreply.github.com>
2023-03-01 18:42:32 +0300

    deleted test folder

--- intro_func/main.cpp
+++ intro_func/main.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
-#include<cstdlib>
 using namespace std;
 
 void sayHi()
 {
     cout << "Hi\n";
-    // return statement isn't needed
+    // return statement isn't needed 
+    return;
+}
+
+int getFiveNumber()
+{
+    return 5;
 }
 
 int main()
 {
     sayHi();
-    
+    // functions that provides some value can be used in cin and cout
+    cout << getFiveNumber();
+
     return 0;
 }
