9903ade79e46b0b1991832ddb006e2c197cfe593
htrq <119476945+htrq@users.noreply.github.com>
2023-03-08 18:10:47 +0300

    simple example of fucntion prototype

---
+++ chapter_0-2/forward_declaration/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int add(int a, int b);  // it's forward declaration or function prototype
+// int add(int, int); also would be valid declaration
+
+int main() 
+{
+    cout << add(3,4) << "\n";
+    float a{};
+    cin >> a;
+    cout << a << "\n";
+    return 0;
+}
+
+int add(int a, int b) 
+{
+    return a + b;
+}
