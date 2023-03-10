a0eb4b739143ef6afa57f5d648ed8978e6f9f004
htrq <119476945+htrq@users.noreply.github.com>
2023-03-10 08:57:30 +0300

    program exit with multiple def error

---
+++ chapter_0-2/multiple_files_in_project_2/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void myFunc()
+{
+    cout << "Hi\n";
+}
+
+int main()
+{
+    return 0;
+}
---
+++ chapter_0-2/multiple_files_in_project_2/myFunc.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+void myFunc()
+{
+    cout << "Hi\n";
+}
