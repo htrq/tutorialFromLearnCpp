176a502e9fc5c0667fd509919327774239b6ce73
htrq <119476945+htrq@users.noreply.github.com>
2023-03-12 19:52:34 +0300

    added add function

--- chapter_0-2/header_guard/main.cpp
+++ chapter_0-2/header_guard/main.cpp
@@ -1,3 +1,4 @@
+#include "add.h"
 #include "geometry.h"
 #include "square.h"
 #include<iostream>
@@ -5,6 +6,7 @@
 int main()
 {
     std::cout << getSquareSides() << "\n";
+    std::cout << add(3, 5) << "\n";
     
     return 0;
 }
