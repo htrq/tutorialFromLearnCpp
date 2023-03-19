bdc3122af4a6c45b510a64d20320929027db33b0
htrq <119476945+htrq@users.noreply.github.com>
2023-03-19 11:12:03 +0300

    simple debugging example done

--- chapter_3_debugging/ctch_er_exmpl.cpp
+++ chapter_3_debugging/ctch_er_exmpl.cpp
@@ -9,7 +9,9 @@ std::cerr << "getFiveValue() called\n"; // this is useful to not indent in error
 int main()
 {
 std::cerr << "main() called\n";
-    std::cout << getFiveValue() << '\n'; // little advise to set single quotes for only special charecters like \n
+    std::cout << getFiveValue << '\n'; // little advise to set single quotes for only special charecters like \n
 
     return 0;
 }
+
+// after adding parentesis to the getFiveValue we can remove tamporary debugging statements
