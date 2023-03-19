3ec3ee4524add59101fc525aa2322c786da8cf29
htrq <119476945+htrq@users.noreply.github.com>
2023-03-19 11:09:31 +0300

    '\n' only in single quotes

--- chapter_3_debugging/ctch_er_exmpl.cpp
+++ chapter_3_debugging/ctch_er_exmpl.cpp
@@ -2,14 +2,14 @@
 
 int getFiveValue()
 {
-std::cerr << "getFiveValue called\n"; // this is useful to not indent in error messages
+std::cerr << "getFiveValue() called\n"; // this is useful to not indent in error messages
     return 5;
 }
 
 int main()
 {
 std::cerr << "main() called\n";
-    std::cout << getFiveValue << "\n";
+    std::cout << getFiveValue() << '\n'; // little advise to set single quotes for only special charecters like \n
 
     return 0;
 }
