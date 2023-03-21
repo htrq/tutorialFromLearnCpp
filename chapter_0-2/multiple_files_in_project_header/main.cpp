95d957bb678c82037029601f7a0a8d3747a0986c
htrq <119476945+htrq@users.noreply.github.com>
2023-03-21 13:40:29 +0300

    define debug flushes buffer cout instantly

--- chapter_0-2/multiple_files_in_project_header/main.cpp
+++ chapter_0-2/multiple_files_in_project_header/main.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include "add.h"
+#define DEBUG
 
 int main()
 {
+    // #ifdef DEBUG
+    std::cout << std::unitbuf;  // enable automatic flushing for std::cout (for debugging)
+    // #endif
     std::cout << add(3,5) << "\n";
     return 0;
 }
