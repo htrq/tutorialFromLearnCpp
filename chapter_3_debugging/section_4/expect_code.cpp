9f83e61db34285b9f5003b098a9f13447c1838d0
htrq <119476945+htrq@users.noreply.github.com>
2023-03-27 16:42:24 +0500

    checking values under cursor example

---
+++ chapter_3_debugging/section_4/expect_code.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main()
+{
+    int x{ 1 };
+    std::cout << x << "\n";
+
+    x += 2;
+    std::cout << x << "\n";
+
+    x += 3;
+    std::cout << x << "\n";
+
+    return 0;
+}
