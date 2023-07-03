428acf72e57f523f389f28eb3eab94404d3b868f
htrq <119476945+htrq@users.noreply.github.com>
2023-07-03 13:43:48 +0500

    added macros with my name

--- chapter_0-2/multiple_files_in_project_header/main.cpp
+++ chapter_0-2/multiple_files_in_project_header/main.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include "add.h"
 #define DEBUG
+#define MY_NAME "Misha"
 
 int main()
 {
     // #ifdef DEBUG
     std::cout << std::unitbuf;  // enable automatic flushing for std::cout (for debugging)
     // #endif
-    std::cout << add(3,5) << "\n";
+    std::cout << MY_NAME << " counted: " << add(3,5) << "\n";
     return 0;
 }
