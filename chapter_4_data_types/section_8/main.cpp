e92bdbb671d53f0d1dab068e24ba583346710758
htrq <119476945+htrq@users.noreply.github.com>
2023-11-09 22:36:58 +0500

    posinf neginf NaN examples

--- chapter_4_data_types/section_8/main.cpp
+++ chapter_4_data_types/section_8/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
@@ -11,10 +12,25 @@ int main()
     std::cout << z << "\n";
     std::cout << 553544321.86 << "\n";  // this literal will be printed in scientific notation (5.53544e+08)
 
-    float valuef{ 545.120695043f }; // this value's output will be with 6 significat digits
+
+    std::cout << std::setprecision(17);
+    float valuef{ 545.120695043f }; // this value's output without setprecision will be with 6 significat digits 
     std::cout << valuef << "\n";
     double valued{ 5.897654120695043 }; // this too
     std::cout << valued << "\n";
+
+    std::cout << 112233445566.7788f << "\n";
+
+    double zero {0.0};
+    double posinf { 6.0 / zero };   // thanks to compiler we can see this positive infinity
+    std::cout << posinf << "\n";
+
+    double neginf { -6.0 / zero}; // this is negative infinity
+    std::cout << neginf << "\n";
+
+    double nan { zero / zero }; // and this one is "not a number"
+    std::cout << nan << "\n";
+
 
     return 0;
 }
