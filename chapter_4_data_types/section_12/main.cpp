a1aa6d8d130865ca1db31824dbeea49d5c7aef8f
htrq <htrq@github.com>
2024-01-24 14:53:55 +0500

    finnaly codeblocks set up

--- chapter_4_data_types/section_12/main.cpp
+++ chapter_4_data_types/section_12/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdint>
 
-void print(int x)
+void print1(int x)
 {
     std::cout << x << "\n";
 }
@@ -9,7 +9,7 @@ void print(int x)
 int main()
 {
     double y { 5.4 };
-    print(y);   // no potential loss data warning whatsoever
+    print1(y);   // no potential loss data warning whatsoever
 
     char letter { 97 };
     std::cout << letter << " has value of " << static_cast<int>(letter) << "\n";    // this is example of use static_cast to translate char into integer type of data
@@ -17,8 +17,8 @@ int main()
     unsigned int joke { -5 };   // this bro doesnt throw any warnings on that, so you should be careful with unsighned and sighned data types
     std::cout << joke << "\n";
 
-    std::int8_t myWierdInt{65}; // and this bro outputs char because he treat it like char, not int!
-    std::cout << myWierdInt << "\n";    // output: A
+    std::int8_t intReconAsChar { 65 }; // and this bro outputs char because he treat it like char, not int!
+    std::cout << intReconAsChar << "\n";    // output: A
 
 
     //task 1
