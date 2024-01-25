31052bfe3734afedd00b7992422c1089ff88ede0
htrq <mikhail.kiselevk@vk.com>
2024-01-25 20:17:37 +0500

    const writes with k and maybe used for parameters

---
+++ chapter_5_constants_and_strings/section1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int printInt(const int x)   //  const may be used for parameters as well
+{
+    return x;
+}
+
+int main()
+{
+    const int8_t kGravityEarth { 9.8 }; // its more common to declare const variables witk "k" prefix
+
+
+
+    return 0;
+}
