e440c7a31abf35f475a5bca917b1b970f86fdf79
Mikhail Kiselev <119476945+htrq@users.noreply.github.com>
2024-02-03 21:26:00 +0500

    formating corrections, removed using namespace std

--- chapter_0-2/unnamed_project/main.cpp
+++ chapter_0-2/unnamed_project/main.cpp
@@ -1,14 +1,13 @@
 #include<iostream>
-using namespace std;
 
 int main() {
-    int x{ };
+    int x{};
 
-    cout << "Hello " "world\n" "Hello world\n"; //it also work with many quotes
-    cout << "Enter number value: ";
-    cin >> x;
-    cout << "Multiplied by 2 entered value is equal to: " << x * 2 << "\n";
-    cout << "Multiplied by 3 entered value is equal to: " << x * 3 << "\n";
+    std::cout << "Hello " "world\n" "Hello world\n"; //it also work with many quotes
+    std::cout << "Enter number value: ";
+    std::cin >> x;
+    std::cout << "Multiplied by 2 entered value is equal to: " << x * 2 << "\n";
+    std::cout << "Multiplied by 3 entered value is equal to: " << x * 3 << "\n";
 
     return 0;
 }
