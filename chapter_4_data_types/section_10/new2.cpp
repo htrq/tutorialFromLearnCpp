d0359c981ddbdbbff463132a1116ae8de3852ee1
htrq <mikhail.kiselevk@vk.com>
2024-02-04 22:41:17 +0500

    code optimized and re-compiled

--- chapter_4_data_types/section_10/new2.cpp
+++ chapter_4_data_types/section_10/new2.cpp
@@ -2,11 +2,11 @@
 
 bool isAllowedToRide()
 {
-    double height{};
     std::cout << "Enter your height (cm): ";
+    double height{};
     std::cin >> height;
-    return (height >= 140);
 
+    return (height >= 140);
 }
 
 int main()
@@ -14,6 +14,7 @@ int main()
     if (isAllowedToRide())
         std::cout << "Welcome in buddy!\n";
     else
-        std::cout << "Youre too short :-( \\n";
+        std::cout << "Youre too short :-(\n";
+
     return 0;
 }
