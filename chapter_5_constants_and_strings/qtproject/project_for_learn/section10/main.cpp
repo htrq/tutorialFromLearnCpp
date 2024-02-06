5f65f0b35762752902165c9862072ca6364b1c11
htrq <mikhail.kiselevk@vk.com>
2024-02-07 00:37:57 +0500

    string_view fast for readonly

--- chapter_5_constants_and_strings/qtproject/project_for_learn/section10/main.cpp
+++ chapter_5_constants_and_strings/qtproject/project_for_learn/section10/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
+#include <string_view>
 
-using namespace std;
+void printStringView(std::string_view str)
+{
+    std::cout << str << '\n';
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    std::string some_variable{ "text here " };   // copy initialzation is slow unlike fundamential variables
+    std::cout << some_variable << '\n';
+
+    std::string_view some_variable2{ "text here 2" };
+    printStringView(some_variable2);    // we're not creating copies of the string so its more efficient
+    // best practice is to prefer string_view over string when you need just read-only variable
+
+    std::string_view some_variable3{ some_variable };   // can be initalized with string
+    std::cout << some_variable3 << '\n';
+
     return 0;
 }
