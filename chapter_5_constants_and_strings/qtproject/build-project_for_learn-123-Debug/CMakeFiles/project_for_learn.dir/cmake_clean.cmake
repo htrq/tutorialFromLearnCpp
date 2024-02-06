01446e26b0e96098ee7e20a416608122249458a8
htrq <mikhail.kiselevk@vk.com>
2024-02-07 00:09:51 +0500

    created project on qtcreator

---
+++ chapter_5_constants_and_strings/qtproject/build-project_for_learn-123-Debug/CMakeFiles/project_for_learn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_for_learn.dir/section10/main.cpp.o"
+  "CMakeFiles/project_for_learn.dir/section10/main.cpp.o.d"
+  "project_for_learn"
+  "project_for_learn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_for_learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter_5_constants_and_strings/qtproject/build-project_for_learn-123-Debug/CMakeFiles/project_for_learn.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for project_for_learn.
+# This may be replaced when dependencies are built.
