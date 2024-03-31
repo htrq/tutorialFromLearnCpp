0380e22db8ddb60b6ce9a2c8e3546c5f8301cf96
htrq <mikhail.kiselevk@vk.com>
2024-03-31 16:46:02 +0500

    input checking working now

--- chapter_9_error_handling/chapter_9_error_handling/main.cpp
+++ chapter_9_error_handling/chapter_9_error_handling/main.cpp
@@ -6,35 +6,39 @@
 void ignore_line()
 {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cin.clear();
 }
 
 int main()
 {
     static_assert(sizeof(int16_t) == 2, "int16_t have to be 2 bytes");  // example of static_assert works during compile time
     std::cout << "Enter number (1 - cin handling example, 2 - assert example): ";
-    int16_t input{};
-    std::cin >> input;
-    if (!std::cin)
+
+    while (true)
     {
-        ignore_line();
-        std::cout << "incorrect input\nTry again: ";
-    }
-    switch (input)
-    {
-    case 1:
-    {
-        getDouble();
-        std::cout << "Enter operation for calculator (+, -, *, /): ";
-        char operation{get_operator()};
-        return 0;
-    }
-    case 2:
-        assert_example();
-        return 0;
-    default:
-        std::cout << "incorrect number\nTry again: ";
-        return 0;
+        int16_t input{};
+        std::cin >> input;
+        if (!std::cin)
+        {
+            std::cout << "incorrect input\nTry again: ";
+            std::cin.clear();
+            ignore_line();
+            continue;
+        }
+        switch (input)
+        {
+        case 1:
+        {
+            getDouble();
+            std::cout << "Enter operation for calculator (+, -, *, /): ";
+            char operation{get_operator()};
+            return 0;
+        }
+        case 2:
+            assert_example();
+            return 0;
+        default:
+            std::cout << "incorrect number\nTry again: ";
+        }
     }
 
     //cin_handling(); // if you enter 9f 9 extracts successfully and other variables take zero value
