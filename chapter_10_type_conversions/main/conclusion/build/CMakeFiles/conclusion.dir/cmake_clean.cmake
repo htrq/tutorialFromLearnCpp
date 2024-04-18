9b3c17dd026e39d2efe6ce2e564dab5aaf201513
htrq <mikhail.kiselevk@vk.com>
2024-04-18 11:42:25 +0500

    created independent project manually

---
+++ chapter_10_type_conversions/main/conclusion/build/CMakeFiles/conclusion.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/conclusion.dir/src/conclusion.cpp.o"
+  "CMakeFiles/conclusion.dir/src/conclusion.cpp.o.d"
+  "conclusion"
+  "conclusion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/conclusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter_10_type_conversions/main/conclusion/include/conclusion.h
@@ -0,0 +1,9 @@
+#ifndef CONCLUSION_H
+#define CONCLUSION_H
+
+double convertToRadians(double define);
+
+void conclusion();
+
+
+#endif
