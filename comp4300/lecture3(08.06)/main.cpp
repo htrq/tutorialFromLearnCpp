dd0198b64a15296c07c3a445f691b72e2fdcc041
htrq <119476945+htrq@users.noreply.github.com>
2024-06-10 17:04:02 +0500

    array is a pointer

--- comp4300/lecture3(08.06)/main.cpp
+++ comp4300/lecture3(08.06)/main.cpp
@@ -6,6 +6,10 @@ int main(int argс, char * argv[])
 	arr[0] = 1;
 	arr[1] = 11;
 	*(arr+2) = 111; 	// is the same as arr[2] = 111 because arrays are pointers
-	std::cout << arr+2 << '\n';	// shows that array is an address
+	std::cout << "expression \"arr + 2 shows\": " << arr+2 << '\n';	// shows that array is an address
+
+	for (int a : arr)
+		std::cout << a << '\n';
+
 	return 0;
 }
